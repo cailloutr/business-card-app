0ff34e37ff2106e5b21f5d342076afc4950680b3
cailloutr <caio.trocilo@gmail.com>
2022-06-25 16:54:29 -0300

    Fix bug in the Pick a color function

--- app/src/main/java/com/example/businesscard/ui/ColorPickerFragment.kt
+++ app/src/main/java/com/example/businesscard/ui/ColorPickerFragment.kt
@@ -5,12 +5,13 @@ import android.app.Dialog
 import android.content.DialogInterface
 import android.graphics.Color
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
 import android.util.Log
 import android.widget.TextView
 import androidx.appcompat.app.AlertDialog
 import androidx.fragment.app.DialogFragment
 import com.example.businesscard.R
-import com.google.android.material.slider.RangeSlider
 import com.google.android.material.slider.Slider
 import com.google.android.material.textfield.TextInputEditText
 
@@ -25,7 +26,7 @@ class ColorPickerFragment : DialogFragment() {
     private lateinit var tvColorView: TextView
 
     private lateinit var listener: ColorPickerDialogListener
-    var colorHex: String = "0"
+    private var colorHex: String = "0"
 
     override fun onCreateDialog(savedInstanceState: Bundle?): Dialog {
         return activity?.let {
@@ -38,41 +39,65 @@ class ColorPickerFragment : DialogFragment() {
             builder.setView(view)
                 // Add action buttons
                 .setPositiveButton("Confirm",
-                    DialogInterface.OnClickListener { dialog, id ->
+                    DialogInterface.OnClickListener { _, _ ->
                         applyColor()
                         listener.applyColor(colorHex)
                     })
                 .setNegativeButton("Cancel",
-                    DialogInterface.OnClickListener { dialog, id ->
-                        getDialog()?.cancel()
+                    DialogInterface.OnClickListener { _, _ ->
+                        dialog?.cancel()
                     })
 
             sliderRed = view.findViewById(R.id.sld_red)
-            sliderRed.addOnChangeListener { slider, value, fromUser ->
-                edtRed.setText(value.toString())
-                applyColor()
-            }
-
             sliderGreen = view.findViewById(R.id.sld_green)
-            sliderGreen.addOnChangeListener { slider, value, fromUser ->
-                edtGreen.setText(value.toString())
-                applyColor()
-            }
-
             sliderBlue = view.findViewById(R.id.sld_blue)
-            sliderBlue.addOnChangeListener { slider, value, fromUser ->
-                edtBlue.setText(value.toString())
-                applyColor()
-            }
 
             edtRed = view.findViewById(R.id.input_red)
             edtGreen = view.findViewById(R.id.input_green)
             edtBlue = view.findViewById(R.id.input_blue)
             tvColorView = view.findViewById(R.id.tv_color_view)
 
+            insertListeners(sliderRed, edtRed)
+            insertListeners(sliderGreen, edtGreen)
+            insertListeners(sliderBlue, edtBlue)
+
             builder.create()
         } ?: throw IllegalStateException("Activity cannot be null")
     }
+
+    private fun insertListeners(sld: Slider, editText: TextInputEditText) {
+        sld.addOnChangeListener { _, value, _ ->
+            editText.setText(value.toInt().toString())
+            applyColor()
+        }
+
+        editText.addTextChangedListener(object : TextWatcher {
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {}
+
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+                var value = if (s.isNullOrEmpty()) 0 else Integer.parseInt(s.toString())
+                when {
+                    value in 0..255 -> {
+                        sld.value = value.toFloat()
+                    }
+                    value <= 0 -> {
+                        value = 0
+                        sld.value = value.toFloat()
+                        editText.setText(value.toString())
+                    }
+                    value > 255 -> {
+                        value = 255
+                        sld.value = value.toFloat()
+                        editText.setText(value.toString())
+                    }
+                }
+            }
+
+            override fun afterTextChanged(s: Editable?) {}
+
+        })
+    }
+
 
     private fun applyColor() {
         val red = edtRed.text.toString().toFloatOrNull()?.toInt()
@@ -86,6 +111,7 @@ class ColorPickerFragment : DialogFragment() {
         this.colorHex = color
     }
 
+    @Deprecated("Deprecated in Java")
     override fun onAttach(activity: Activity) {
         super.onAttach(activity)
 
