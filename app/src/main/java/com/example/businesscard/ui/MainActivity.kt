784ff4bd9d4370c6321fcc9c322a282ba8f986fb
cailloutr <caio.trocilo@gmail.com>
2022-06-26 17:10:15 -0300

    Created adapter and viewModel for displaying the cards on the recyclerview

--- app/src/main/java/com/example/businesscard/ui/MainActivity.kt
+++ app/src/main/java/com/example/businesscard/ui/MainActivity.kt
@@ -2,18 +2,28 @@ package com.example.businesscard.ui
 
 import android.content.Intent
 import android.os.Bundle
+import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
+import com.example.businesscard.App
 import com.example.businesscard.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
-    lateinit var binding: ActivityMainBinding
+    private lateinit var binding: ActivityMainBinding
+    
+    private val adapter by lazy { BusinessCardAdapter() }
+
+    private val mainViewModel: MainViewModel by viewModels {
+        MainViewModelFactory((application as App).repository)
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        binding.rvCards.adapter = adapter
+        getAllBusinessCard()
         insertListeners()
     }
 
@@ -21,6 +31,12 @@ class MainActivity : AppCompatActivity() {
         binding.btnAddNewCard.setOnClickListener {
             val intent = Intent(this@MainActivity, AddBusinessCardActivity::class.java)
             startActivity(intent)
+        }
+    }
+
+    private fun getAllBusinessCard() {
+        mainViewModel.getAll().observe(this) { businessCards ->
+            adapter.submitList(businessCards)
         }
     }
 
